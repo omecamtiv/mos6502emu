29780850b31f2a98c9b6737035ae2d05df2134e8
sudipta-nandan <nandan.sudipta@gmail.com>
2021-04-24 03:22:50 +0530

    UI dev started.

--- include/ncurs/ui.hpp
+++ include/ncurs/ui.hpp
@@ -2,18 +2,17 @@
 #define UI_HPP
 
 #include <ncurses.h>
-#include <panel.h>
 
 class UI {
-       public:
-	WINDOW *base_win, *status_win, *ram_win, *menu_win, *cmd_win;
-	PANEL *base_panel, *status_panel, *ram_panel, *menu_panel, *cmd_panel;
-	void initui();
-	void init_base(WINDOW *win, WINDOW *parent_win);
+	void init_base();
 	void init_status(WINDOW *win, WINDOW *parent_win);
+	void init_cmd(WINDOW *win, WINDOW *parent_win);
 	void init_ram(WINDOW *win, WINDOW *parent_win);
 	void init_menu(WINDOW *win, WINDOW *parent_win);
-	void init_cmd(WINDOW *win, WINDOW *parent_win);
+
+       public:
+	WINDOW *base_win, *status_win, *ram_win, *menu_win, *cmd_win;
+	void initui();
 };
 
 #endif
--- src/ncurs/ui.cpp
+++ src/ncurs/ui.cpp
@@ -5,33 +5,84 @@
 #include <cstring>
 
 void UI::initui() {
+	int c;
+	char char_q = 'q';
 	initscr();
 	cbreak();
 	noecho();
 	curs_set(0);
-
-	init_base(base_win, stdscr);
-
-	getch();
+	init_base();
+	while (c != char_q) {
+		c = getch();
+		switch (c) {
+			case KEY_RESIZE:
+				init_base();
+				break;
+			default:;
+		}
+	}
 	endwin();
 	return;
 }
 
-void UI::init_base(WINDOW *win, WINDOW *parent_win) {
-	int row, col, start_y, start_x;
-	const int ROW = 40;
-	const int COL = 140;
-	getmaxyx(parent_win, row, col);
-	start_y = row / 2 - ROW / 2;
-	start_x = col / 2 - COL / 2;
-	win = newwin(ROW, COL, start_y, start_x);
+void UI::init_base() {
+	int row, col, win_row, win_col, start_y, start_x;
+	getmaxyx(stdscr, row, col);
+	win_col = col - (col % 8);
+	win_row = (win_col / 8) * 3;
+	while (win_row > row || win_col > col) {
+		win_row -= 8;
+		win_col -= 3;
+	}
+	start_y = row / 2 - win_row / 2;
+	start_x = col / 2 - win_col / 2;
+	clear();
+	base_win = newwin(win_row, win_col, start_y, start_x);
 	refresh();
+	box(base_win, 0, 0);
+	init_status(status_win, base_win);
+	init_cmd(cmd_win, base_win);
+	return;
+}
+
+void UI::init_status(WINDOW *win, WINDOW *parent_win) {
+	int row, col, win_row, win_col, start_y, start_x;
+	getmaxyx(parent_win, row, col);
+	win_row = 5;
+	win_col = 26;
+	start_y = 2;
+	start_x = 1;
+	win = derwin(parent_win, win_row, win_col, start_y, start_x);
+	wrefresh(parent_win);
 	box(win, 0, 0);
-	const char BASE_HEADING[] = "MOS-6502  EMULATOR";
-	int len = strlen(BASE_HEADING);
-	int pos_x = COL / 2 - len / 2;
-	int pos_y = 1;
-	mvwprintw(win, pos_y, pos_x, BASE_HEADING);
+	wrefresh(win);
+	return;
+}
+
+void UI::init_cmd(WINDOW *win, WINDOW *parent_win) {
+	int row, col, win_row, win_col, start_y, start_x;
+	getmaxyx(parent_win, row, col);
+	win_row = 3;
+	win_col = col - 2;
+	start_y = row - 4;
+	start_x = 1;
+	win = derwin(parent_win, win_row, win_col, start_y, start_x);
+	wrefresh(parent_win);
+	box(win, 0, 0);
+	wrefresh(win);
+	return;
+}
+
+void UI::init_ram(WINDOW *win, WINDOW *parent_win) {
+	int row, col, win_row, win_col, start_y, start_x;
+	getmaxyx(win, row, col);
+	win_row = 3;
+	win_col = col - 2;
+	start_y = row - 4;
+	start_x = 1;
+	win = derwin(parent_win, win_row, win_col, start_y, start_x);
+	wrefresh(parent_win);
+	box(parent_win, 0, 0);
 	wrefresh(win);
 	return;
 }
